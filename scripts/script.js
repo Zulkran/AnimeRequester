f64f7ff00a8f5a56484cea4896895b93dcb6a2cb
Mary Ilhan <ilhan.mary@etu.unicaen.fr>
2025-09-30 12:56:05 +0200

    API finish V1

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,13 @@
 let typeSearch;
-let input = document.getElementById("input");
-let searchButton = document.getElementById("searchButton");
+let inputSearch;
+const searchButton = document.getElementById("searchButton");
+const eraseButton = document.getElementById("eraseButton");
 
 async function animeSearch() {
     let url;
     typeSearch = document.getElementById('category').value;
+    inputSearch = document.getElementById("site-search").value;
+    console.log(inputSearch);
     const options = {
         method: 'GET',
         headers: {
@@ -14,41 +17,57 @@ async function animeSearch() {
     };
 
     if (typeSearch == 'title') {
-        url = `https://anime-db.p.rapidapi.com/anime?page=1&size=10&search=${input.textContent}`;
-
+        url = `https://anime-db.p.rapidapi.com/anime?page=1&size=10&search=${inputSearch}`;
     }
     else if (typeSearch == 'type') {
-        url = `https://anime-db.p.rapidapi.com/${input.textContent}`
+        url = `https://anime-db.p.rapidapi.com/${inputSearch}`;
     }
-    else if (typeSearch == 2) {
-        url = `https://anime-db.p.rapidapi.com/anime/by-id/${input.textContent}`
+    else if (typeSearch == 'byId') {
+        url = `https://anime-db.p.rapidapi.com/anime/by-id/${parseInt(inputSearch)}`;
     }
-    else if (typeSearch == 3) {
-        url = `https://anime-db.p.rapidapi.com/anime/by-ranking/${input.textContent}`
+    else if (typeSearch == 'rank') {
+        url = `https://anime-db.p.rapidapi.com/anime/by-ranking/${parseInt(inputSearch)}`;
     }
-
     try {
         const response = await fetch(url, options);
-        const result = await response.json();
+        const result = (await response.json());
         console.log(result);
+        return result;
     } catch (error) {
         console.error(error);
     }
 }
 
 async function findAnime(){
-    result;
+    clearAnime();
+    let result = await animeSearch();
+    if (typeSearch == 'title') {
+        result = result.data;
+        for(let data of result) {
+            addAnimeCard(data.title, data.image, data.synopsis, data.genres, data.ranking, data.episodes);
+        }
+    }
+    else {
+        addAnimeCard(result.title, result.image, result.synopsis, result.genres, result.ranking, result.episodes);
+    }
 }
 
-function addAnimeCard(title, imgPath, desc){
+function addAnimeCard(title, imgPath, desc, type, rank, nbrEpisode){
     document.getElementById('anime-content').innerHTML 
     += 
-    `<div id= ""class="bg-gray-100 rounded-lg m-4 text-center p-2 shadow-lg">
-        <h1>${title}<\h1>
+    `<div class="justify-center text-center align-center bg-gray-100 rounded-lg m-4 p-2 shadow-lg">
+        <h1 class="font-bold">${title}<\h1>
         <img src = "${imgPath}"\>
-        <p>${desc}<\p>
-    </div>`
+        <p>The synopsis is : ${desc}<\p>
+        <p>Genre : ${type}<\p>
+        <p>Rank : ${rank}<\p>
+        <p>Number of episode : ${nbrEpisode}<\p>
+    </div>`;
+}
+
+function clearAnime() {
+    document.getElementById('anime-content').innerHTML = null;
 }
 
 searchButton.addEventListener("click", () => findAnime());
-
+eraseButton.addEventListener("click", () => clearAnime());
