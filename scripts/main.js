1e97cdda3738846baaea6b8c5de32682533b5e53
Mary Ilhan <ilhan.mary@etu.unicaen.fr>
2025-10-15 16:09:55 +0200

    feat : add display gender

--- scripts/main.js
+++ scripts/main.js
@@ -1,20 +1,9 @@
 import { searchButton,eraseButton,animeSearch,findAnime } from './api_searchAnime.js';
 import { darkButton,initialiseDark, toggleTheme } from './darkMode.js';
 import { APIButton, APIPrompt, openClosePrompt,updateAPI,updateAPIButton,inputAPIKey } from './APIKeyButton.js';
-import {animeContent, addAnimeCard, clearAnime} from './addCard.js';
+import {animeContent, addAnimeCard, clearAnime} from './addHTMLFunction.js';
 
 initialiseDark();
-
-// Using prompt to get api key
-if(sessionStorage.getItem('userKey') == null || sessionStorage.getItem('userKey').length < 1) {
-    let userKey = prompt("Please enter your api key:");
-    sessionStorage.setItem('userKey', userKey);
-    if (userKey !== null) {
-      alert(`Hello you have access to your application!`);
-    } else {
-      alert("You canceled the input.");
-    }
-}
 
 searchButton.addEventListener("click", () => findAnime());
 eraseButton.addEventListener("click", () => clearAnime());
